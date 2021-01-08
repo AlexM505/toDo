4d3a44619c9e4c99e20f8f2a51709ea152dfc712
cmarenco <cmarenco@thedataage.com>
2021-01-08 16:48:12 -0600

    Funcionalidad ActualizarItem (BD)

--- app/src/main/java/com/alxd/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/alxd/todoapp/fragments/update/UpdateFragment.kt
@@ -2,12 +2,17 @@ package com.alxd.todoapp.fragments.update
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.alxd.todoapp.R
 import com.alxd.todoapp.data.models.Priority
+import com.alxd.todoapp.data.models.ToDoData
+import com.alxd.todoapp.data.viewmodel.ToDoViewModel
 import com.alxd.todoapp.fragments.SharedViewModel
+import kotlinx.android.synthetic.main.fragment_update.*
 import kotlinx.android.synthetic.main.fragment_update.view.*
 
 class UpdateFragment : Fragment() {
@@ -15,6 +20,7 @@ class UpdateFragment : Fragment() {
     private val args by navArgs<UpdateFragmentArgs>()
 
     private val mSharedViewModel: SharedViewModel by viewModels()
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +33,7 @@ class UpdateFragment : Fragment() {
 
         view.etCurrentTitle.setText(args.currentItem.title)
         view.etCurrentDescription.setText(args.currentItem.description)
-        view.spinnerCurrentPriorities.setSelection(parsePriority(args.currentItem.priority))
+        view.spinnerCurrentPriorities.setSelection(mSharedViewModel.parsePriorityToInt(args.currentItem.priority))
         view.spinnerCurrentPriorities.onItemSelectedListener = mSharedViewModel.listener
 
         return view
@@ -37,11 +43,36 @@ class UpdateFragment : Fragment() {
         inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
-    private fun parsePriority(priority: Priority): Int{
-        return when(priority){
-            Priority.HIGH -> 0
-            Priority.MEDIUM -> 1
-            Priority.LOW -> 2
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.menu_save){
+            updateItem()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateItem(){
+        val title = etCurrentTitle.text.toString()
+        val desc = etCurrentDescription.text.toString()
+        val getPriority = spinnerCurrentPriorities.selectedItem.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(title, desc)
+        if(validation){
+            //Actualizando a base de datos
+            val updatedItem = ToDoData(
+                    args.currentItem.id,
+                    title,
+                    mSharedViewModel.parsePriority(getPriority),
+                    desc
+            )
+
+            mToDoViewModel.updateData(updatedItem)
+            Toast.makeText(requireContext(),"Se ha actualizado correctamente!",Toast.LENGTH_SHORT).show()
+
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        }else{
+            Toast.makeText(requireContext(), "Por favor ingrese texto en todos los campos", Toast.LENGTH_SHORT).show()
         }
     }
 
